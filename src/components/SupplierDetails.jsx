fb915e2f92a9791e5c5816539d73d01b98d2acb0
Avinash Kumar <avinash.kumar@embitel.com>
2023-10-20 13:18:08 +0530

    add supplier details responsive

--- src/components/SupplierDetails.jsx
+++ src/components/SupplierDetails.jsx
@@ -2,12 +2,14 @@ import { SUPPLIERS_DETAILS } from "../data/order-data";
 
 const SupplierDetails = () => {
   return (
-    <div className="flex rounded-2xl bg-white divide-x-2 py-5 px-14 w-full justify-center mb-4">
+    <div className="bg-white p-5 md:rounded-2xl md:divide-x-2 md:flex md:w-full md:justify-center md:mb-4">
       {SUPPLIERS_DETAILS.map((detail) => (
-        <div key={detail.label} className="flex flex-col basis-1/6">
+        <div key={detail.label} className="w-full md:w-1/2 lg:w-1/4 xl:w-1/6">
           <div className="mx-auto ml-5">
             <p className="text-gray-500 font-medium mb-1">{detail.label}</p>
-            <p className="text-black font-semibold text-lg">{detail.text}</p>
+            <p className="text-black font-semibold text-md md:text-lg lg:text-lg xl:text-lg">
+              {detail.text}
+            </p>
           </div>
         </div>
       ))}
